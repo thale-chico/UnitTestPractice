82fac34887ade7ce6ffa727f204abdf0165c53a1
Tristen Hale <tahale1@csuchico.edu>
2026-04-13 11:36:31 -0700

    Update PasswordTest.cpp

--- PasswordTest.cpp
+++ PasswordTest.cpp
@@ -14,9 +14,41 @@ class PasswordTest : public ::testing::Test
 		virtual void TearDown(){} //clean up after each test, (before destructor)
 };
 
+// test 'count_leading_characters' function
 TEST(PasswordTest, single_letter_password)
 {
 	Password my_password;
-	int actual = my_password.count_leading_characters("Z");
-	ASSERT_EQ(1, actual);
+	ASSERT_EQ(1, my_password.count_leading_characters("Z"));
+}
+TEST(PasswordTest, mixed_case_password)
+{
+	Password my_password;
+	ASSERT_EQ(2, my_password.count_leading_characters("ZZz"));
+}
+TEST(PasswordTest, empty_char_password)
+{
+	Password my_password;
+	ASSERT_EQ(0, my_password.count_leading_characters(""));
+}
+TEST(PasswordTest, duplicate_char_password)
+{
+	Password my_password;
+	ASSERT_EQ(2, my_password.count_leading_characters("aabb"));
+}
+TEST(PasswordTest, mixed_duplicate_char_password)
+{
+	Password my_password;
+	EXPECT_EQ(2, my_password.count_leading_characters("Aabb"));
+	EXPECT_EQ(2, my_password.count_leading_characters("aaBb"));
+}
+// test 'has_mixed_case' function
+TEST(PasswordTest, mixed_char_password_check)
+{
+	Password my_password;
+	EXPECT_TRUE(my_password.has_mixed_case("Aab"));
+}
+TEST(PasswordTest, char_password_check)
+{
+	Password my_password;
+	EXPECT_FALSE(2, my_password.has_mixed_case("ab"));
 }
